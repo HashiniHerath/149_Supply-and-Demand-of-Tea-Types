16312e276ab8659f2d7d987a469bb7936dda7662
Movindu12# <it21309274@my.sliit.lk>
2024-12-08 11:31:49 +0530

    Initial commit

---
+++ api/main.py
@@ -0,0 +1,55 @@
+from fastapi import FastAPI, HTTPException
+from pydantic import BaseModel
+import joblib
+from fastapi.middleware.cors import CORSMiddleware
+import pandas as pd
+from collections import defaultdict
+from typing import Dict, List
+
+
+app = FastAPI()
+
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  # Update this with your frontend's origin for better security
+    allow_credentials=True,
+    allow_methods=["*"],  # Allow all HTTP methods
+    allow_headers=["*"],  # Allow all headers
+)
+
+# Load Models
+sales_model = joblib.load('model_sales_quantity.joblib')
+  
+elevation_map = {'High grown': 0, 'Low grown': 1, 'Mid grown': 2, 'Unknown': 3}
+
+# Pydantic model for Sales Predict
+class PredictionInput(BaseModel):
+    year: float
+    dollar_rate: float
+    elevation: str  # Input elevation as a string
+    avg_price: float
+
+@app.post("/predict/sales-quantity")
+async def predict_sales_quantity(input_data: PredictionInput):
+    
+    try:
+        # Encode elevation
+        elevation_encoded = elevation_map.get(input_data.elevation, elevation_map['Unknown'])
+        
+        model_input = [
+            [
+                input_data.year,
+                input_data.dollar_rate,
+                elevation_encoded,
+                input_data.avg_price
+            ]
+        ]
+        
+
+        prediction = sales_model.predict(model_input)
+        
+        return {"predicted_quantity": prediction[0]}
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=f"Error during prediction: {str(e)}")
+    
