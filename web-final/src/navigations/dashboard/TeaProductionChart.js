6f5671355c3fde04e2e05934876f01bb46732a20
sachindinuja <schin.dinuja28@gmail.com>
2025-05-24 02:30:28 +0530

    update libraries TeaProductionChart file

---
+++ web-final/src/navigations/dashboard/TeaProductionChart.js
@@ -0,0 +1,10 @@
+import React, { useEffect, useState } from 'react';
+import { Bar } from 'react-chartjs-2';
+import { Chart as ChartJS, CategoryScale, LinearScale, BarElement, Title, Tooltip, Legend } from 'chart.js';
+import axios from 'axios';
+import jsPDF from 'jspdf';
+import 'jspdf-autotable';
+import html2canvas from 'html2canvas';
+import { useNavigate } from 'react-router-dom';
+import { FaArrowLeft, FaArrowRight } from 'react-icons/fa';
+import Env from '../../data/Env';
