9175efd3fdcd598a78fd2a578f68232c8445f749
sachindinuja <schin.dinuja28@gmail.com>
2025-05-24 02:59:21 +0530

    add libraries

---
+++ web-final/src/navigations/dashboard/TeaWholeProductionChart.js
@@ -0,0 +1,10 @@
+import { useState, useEffect } from "react";
+import axios from "axios";
+import { Chart as ChartJS, CategoryScale, LinearScale, BarElement, Title, Tooltip, Legend } from "chart.js";
+import { Bar } from "react-chartjs-2";
+import { useNavigate } from "react-router-dom";
+import { FaArrowLeft, FaArrowRight } from "react-icons/fa";
+import jsPDF from 'jspdf';
+import 'jspdf-autotable';
+import html2canvas from 'html2canvas';
+import Env from "../../data/Env";
