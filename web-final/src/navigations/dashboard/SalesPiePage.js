143949db2f2559a2ed34e15c236351a3c83c5acd
Movindu12# <it21309274@my.sliit.lk>
2025-05-24 03:28:45 +0530

    import libraries-sales pie page

---
+++ web-final/src/navigations/dashboard/SalesPiePage.js
@@ -0,0 +1,6 @@
+// pages/SalesPage.js
+import React, { useEffect, useState } from 'react';
+import Footer from '../../components/footer/Footer';
+import Sidebar from '../../components/sidebar/Sidebar';
+import Topbar from '../../components/topbar/Topbar';
+import SalesPieChart from './SalesPieChart';
