a58d9048a86b396afede6ca6cef3cfcc64d4e6ff
sachindinuja <schin.dinuja28@gmail.com>
2025-05-24 03:10:29 +0530

    import libraries

---
+++ web-final/src/navigations/dashboard/TypesChart.js
@@ -0,0 +1,13 @@
+import React, { useEffect, useState } from "react";
+import { Bar } from "react-chartjs-2";
+import {
+  Chart as ChartJS,
+  CategoryScale,
+  LinearScale,
+  BarElement,
+  Title,
+  Tooltip,
+  Legend,
+} from "chart.js";
+import axios from "axios";
+import Env from "../../data/Env";
