695541df5f726409e6b52c7011c7590aa15820f9
Movindu12# <it21309274@my.sliit.lk>
2025-05-24 02:30:18 +0530

    update-sales chart code

--- web-final/src/navigations/dashboard/SalesChart.js
+++ web-final/src/navigations/dashboard/SalesChart.js
@@ -7,4 +7,24 @@ import jsPDF from 'jspdf';
 import 'jspdf-autotable';
 import html2canvas from 'html2canvas';
 import { useNavigate } from 'react-router-dom';
-//import Env from '../../data/Env';
+import Env from '../../data/Env';
+
+ChartJS.register(CategoryScale, LinearScale, BarElement, Title, Tooltip, Legend);
+
+const SalesChart = () => {
+  const navigate = useNavigate()
+  const [salesData, setSalesData] = useState({});
+  const [loading, setLoading] = useState(false);
+  const [selectedElevations, setSelectedElevations] = useState({
+    'High grown': true,
+    'Low grown': false,
+    'Mid grown': false
+  });
+  const [salesCode, setSalesCode] = useState(4);
+  const [dollarRate, setDollarRate] = useState(300);
+  const [avgPrice, setAvgPrice] = useState(1000);
+  const [errors, setErrors] = useState({});
+
+  const handleSalesCodeChange = (code) => {
+    setSalesCode(Number(code));
+  };
