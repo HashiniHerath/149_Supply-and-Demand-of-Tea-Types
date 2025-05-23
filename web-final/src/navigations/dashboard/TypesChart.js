f186b3b716ea6bcef008196a94ba2cab9f813650
sachindinuja <schin.dinuja28@gmail.com>
2025-05-24 03:11:01 +0530

    Registering necessary chart.js components

--- web-final/src/navigations/dashboard/TypesChart.js
+++ web-final/src/navigations/dashboard/TypesChart.js
@@ -11,3 +11,16 @@ import {
 } from "chart.js";
 import axios from "axios";
 import Env from "../../data/Env";
+
+// Registering necessary chart.js components
+ChartJS.register(CategoryScale, LinearScale, BarElement, Title, Tooltip, Legend);
+
+const TypesChart = () => {
+  const [demandData, setDemandData] = useState({});
+  const [loading, setLoading] = useState(false);
+  const [selectedType, setSelectedType] = useState("CTC TEA");
+  const [selectedQuantities, setSelectedQuantities] = useState({
+    HIGH: true,
+    LOW: true,
+    MEDIUM: true,
+  });
