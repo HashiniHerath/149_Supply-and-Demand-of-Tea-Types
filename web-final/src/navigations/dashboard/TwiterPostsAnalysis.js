68915180aad220217468bc35e5880aa6433131b0
HashiniHerath <herathhashi7@gmail.com>
2025-05-24 18:19:37 +0530

    Prepare data for Line Chart

--- web-final/src/navigations/dashboard/TwiterPostsAnalysis.js
+++ web-final/src/navigations/dashboard/TwiterPostsAnalysis.js
@@ -55,4 +55,59 @@ const TwitterPostAnalysisChart = () => {
     fetchPostData();
   }, []);
 
-  
+    const lineChartData = {
+    labels: postAnalysisData.map((item) => item.year),
+    datasets: [
+      {
+        label: "Twitter Posts",
+        data: postAnalysisData.map((item) => item.post_count),
+        borderColor: "blue",
+        backgroundColor: "rgba(0, 123, 255, 0.2)",
+        fill: true,
+        tension: 0.4,
+      },
+    ],
+  };
+
+  const options = {
+    responsive: true,
+    plugins: {
+      legend: {
+        position: "top",
+        labels: {
+          color: "#333",
+        },
+      },
+      title: {
+        display: true,
+        text: "Twitter Post Analysis Over Years",
+        color: "#333",
+        font: {
+          size: 18,
+        },
+      },
+    },
+    scales: {
+      x: {
+        ticks: {
+          color: "#333",
+        },
+        title: {
+          display: true,
+          text: "Year",
+          color: "#333",
+        },
+      },
+      y: {
+        ticks: {
+          color: "#333",
+        },
+        title: {
+          display: true,
+          text: "Number of Posts",
+          color: "#333",
+        },
+        beginAtZero: true,
+      },
+    },
+  };
