7c68ad145e37e2f4687bbc1b8b54a82ef15b07b1
HashiniHerath <herathhashi7@gmail.com>
2025-05-24 15:16:35 +0530

    Add a Sort the counts by date

--- api/main.py
+++ api/main.py
@@ -367,3 +367,5 @@ async def fetch_and_count_keywords_instagram(keywords):
             response_data = json.loads(data.decode("utf-8"))
 
             counts = count_items_by_year_month(response_data)
+
+            sorted_counts = {k: counts[k] for k in sorted(counts)}
