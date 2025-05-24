0fda809153f058cbb982f156716a6498536e566e
HashiniHerath <herathhashi7@gmail.com>
2025-05-24 18:09:38 +0530

    Add PDF generation for Instagram post analysis report with logo, chart, and data table

--- web-final/src/navigations/dashboard/InstagramPostsAnalysis.js
+++ web-final/src/navigations/dashboard/InstagramPostsAnalysis.js
@@ -139,3 +139,50 @@ const InstagramPostAnalysisChart = () => {
       },
     },
   };
+
+  const generatePDF = async () => {
+    const doc = new jsPDF("landscape");
+    const logo = new Image();
+    logo.src = `${process.env.PUBLIC_URL}/images/logo.png`;
+
+    logo.onload = () => {
+      // Add logo and header
+      doc.addImage(logo, "PNG", 10, 10, 50, 30);
+      doc.setFontSize(22);
+      doc.text("TeaVerse", 70, 20);
+      doc.setFontSize(10);
+      doc.text("123 Green Tea Road, Colombo, Sri Lanka", 70, 30);
+      doc.text("Phone: +94 77 123 4567 | Email: contact@teaverse.com", 70, 37);
+      doc.text("Website: www.teaverse.com", 70, 44);
+      doc.setDrawColor(150);
+      doc.line(10, 50, 280, 50);
+      doc.setFontSize(16);
+      doc.text("Instagram Post Analysis Report", 10, 60);
+
+      const chartElement = document.querySelector(".chartContainer3");
+      if (chartElement) {
+        html2canvas(chartElement).then((canvas) => {
+          const imgData = canvas.toDataURL("image/png");
+          doc.addImage(imgData, "PNG", 10, 70, 260, 120);
+
+          doc.setFontSize(14);
+          doc.text("Estimated Post Data Table", 10, 200);
+
+          const tableData = [];
+          Object.keys(postAnalysisData).forEach((teaType) => {
+            Object.entries(postAnalysisData[teaType] || {}).forEach(([year, count]) => {
+              tableData.push([teaType, year, count]);
+            });
+          });
+
+          doc.autoTable({
+            head: [["Tea Type", "Year", "Number of Posts"]],
+            body: tableData,
+            startY: 210,
+          });
+
+          doc.save("Instagram_Post_Analysis_Report.pdf");
+        });
+      }
+    };
+  };
